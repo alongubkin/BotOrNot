a2d7c25de65d36a5435b51f0f37885def9a2a050
Snir David <snir@fastmail.fm>
2015-11-13 20:06:05 +0200

    wip

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -72,15 +72,67 @@ angular.module('ionicApp', ['ionic'])
 
 })
 
-.controller('SignInCtrl', function($scope, $state) {
+.controller('SignInCtrl', function($scope, $state, $ionicModal) {
   
   $scope.signIn = function(user) {
     console.log('Sign-In', user);
     $state.go('tabs.home');
   };
   
+  $ionicModal.fromTemplateUrl('my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+    
+  });
+  
+  $scope.openModal = function() {
+    $scope.modal.show();
+    
+    
+    var selected = [];
+    $(".image").click(function () {
+      var productId = $(this).attr("data-product-id");
+      var index = selected.indexOf(productId);
+      if (index === -1) {
+        selected.push(productId);
+        $(this).addClass("checked");
+      } else {
+        selected.splice(index, 1);
+        $(this).removeClass("checked");
+      }
+
+      if (selected.length === 3 && 
+          selected.indexOf("5") !== -1 && 
+          selected.indexOf("9") !== -1 &&
+          selected.indexOf("11") !== -1) {
+        console.log("success");
+      }
+    })
+    
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+
+
 })
 
 .controller('HomeTabCtrl', function($scope) {
   console.log('HomeTabCtrl');
 });
+
+
